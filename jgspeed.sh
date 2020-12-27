dec97c217b0280f5e43d6f4ca35ee85622cf72a4
jgarr16 <15323251+jgarr16@users.noreply.github.com>
2020-12-26 20:13:12 -0800

    Update jgspeed.sh

--- jgspeed.sh
+++ jgspeed.sh
@@ -2,4 +2,4 @@
 
 output=$(/usr/local/bin/speedtest --server 14204)
 echo "$output" | awk '/Mbit\/s/ || /Testing from/ || /Hosted by/{print}' | nc -w0 -u 192.168.1.108 9994
-echo "done..."
+echo "done with the cron job..."
