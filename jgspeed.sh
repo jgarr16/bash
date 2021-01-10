e332056b209b05d70ddab6b647a384551df19a4c
jgarr16 <15323251+jgarr16@users.noreply.github.com>
2021-01-09 22:34:42 -0800

    Update jgspeed.sh

--- jgspeed.sh
+++ jgspeed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+PATH_CENT="~/bin/"; PATH_MAC="/usr/local/bin/"; PATH_ALP="/usr/bin/"; host=$(hostname); case "$host" in CENTOS|CENT*) PATH_TO_USE="$PATH_CENT";;Johns|Johns*) PATH_TO_USE="$PATH_MAC";; *) PATH_TO_USE="$PATH_ALP" ;; esac; output=$("$PATH_TO_USE"speedtest --server 14204); echo "$output" | awk '/Mbit\/s/ || /Testing from/ || /Hosted by/{print}' 
 
-output=$(/usr/local/bin/speedtest --server 14204)
-echo "$output" | awk '/Mbit\/s/ || /Testing from/ || /Hosted by/{print}' | nc -w0 -u 192.168.1.108 9994
-echo "done with the cron job..."
+
+# | nc -w0 -u 192.168.1.108 9994
