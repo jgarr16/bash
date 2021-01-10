f8c7eba9f4dc768ae45e01933ca3e826a74e7409
jgarr16 <15323251+jgarr16@users.noreply.github.com>
2021-01-09 22:55:39 -0800

    Update jgspeed.sh

--- jgspeed.sh
+++ jgspeed.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-PATH_CENT="~/bin/"; PATH_MAC="/usr/local/bin/"; PATH_ALP="/usr/bin/"; host=$(hostname); case "$host" in CENTOS|CENT*) PATH_TO_USE="$PATH_CENT";;Johns|Johns*) PATH_TO_USE="$PATH_MAC";; *) PATH_TO_USE="$PATH_ALP" ;; esac; output=$("$PATH_TO_USE"speedtest --server 14204); echo "$output" | awk '/Mbit\/s/ || /Testing from/ || /Hosted by/{print}' 
+PATH_CENT="/home/jrgarrigues/bin/"; PATH_MAC="/usr/local/bin/"; PATH_ALP="/usr/bin/"; host=$(hostname); case "$host" in CENTOS|CENT*) PATH_TO_USE="$PATH_CENT";;Johns|Johns*) PATH_TO_USE="$PATH_MAC";; *) PATH_TO_USE="$PATH_ALP" ;; esac; output=$("$PATH_TO_USE"speedtest --server 14204); echo "$output" | awk '/Mbit\/s/ || /Testing from/ || /Hosted by/{print}' | nc -w 1 -u 192.168.1.107 9994
 
 
-# | nc -w0 -u 192.168.1.108 9994
+
+# Depending on the type of outcome you want:
+# | nc -w 1 -u 192.168.1.107 9994
+# > /dev/ttys009
