2230df8b05d575f5a3fd2dd8e263b48964df6902
jgarr16 <15323251+jgarr16@users.noreply.github.com>
2021-01-09 23:48:44 -0800

    Update jgspeed.sh

--- jgspeed.sh
+++ jgspeed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PATH_CENT="/home/jrgarrigues/bin/"; PATH_MAC="/usr/local/bin/"; PATH_ALP="/usr/bin/"; host=$(hostname); case "$host" in CENTOS|CENT*) PATH_TO_USE="$PATH_CENT";;Johns|Johns*) PATH_TO_USE="$PATH_MAC";; *) PATH_TO_USE="$PATH_ALP" ;; esac; output=$("$PATH_TO_USE"speedtest); echo "$output" | awk '/Mbit\/s/ || /Testing from/ || /Hosted by/{print}' | nc -w 1 -u 192.168.1.107 9994
+PATH_CENT="/home/jrgarrigues/bin/"; PATH_MAC="/usr/local/bin/"; PATH_ALP="/usr/bin/"; host=$(hostname); case "$host" in CENTOS|CENT*) PATH_TO_USE="$PATH_CENT";;Johns|Johns*) PATH_TO_USE="$PATH_MAC";; *) PATH_TO_USE="$PATH_ALP" ;; esac; output=$("$PATH_TO_USE"speedtest --server 14204); echo "$output" | awk '/Mbit\/s/ || /Testing from/ || /Hosted by/{print}' | nc -w 1 -u 192.168.1.107 9994
 
 
 
