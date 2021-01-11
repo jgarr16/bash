54a0f977493d8308cedf87c31e2d49d00c3c3f76
jgarr16 <15323251+jgarr16@users.noreply.github.com>
2021-01-10 17:48:56 -0800

    Update jgspeed.sh

--- jgspeed.sh
+++ jgspeed.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-PATH_CENT="/home/jrgarrigues/bin/"; PATH_MAC="/usr/local/bin/"; PATH_ALP="/opt/bin/"; host=$(hostname); case "$host" in CENTOS|CENT*) PATH_TO_USE="$PATH_CENT";;Johns|Johns*) PATH_TO_USE="$PATH_MAC";; *) PATH_TO_USE="$PATH_ALP" ;; esac; output=$("$PATH_TO_USE"speedtest --server 14204); echo "$output" | awk '/Mbit\/s/ || /Testing from/ || /Hosted by/{print}' | netcat -w 1 -u 192.168.1.107 9994
+PATH_CENT="/home/jrgarrigues/bin/"; PATH_MAC="/usr/local/bin/"; PATH_ALP="/opt/bin/"; host=$(hostname); case "$host" in CENTOS|CENT*) PATH_TO_USE="$PATH_CENT";;Johns|Johns*) PATH_TO_USE="$PATH_MAC";; *) PATH_TO_USE="$PATH_ALP" ;; esac; output=$("$PATH_TO_USE"speedtest --server 14204); echo "$output" | awk '/Mbit\/s/ || /Testing from/ || /Hosted by/{print}' | netcat -w=1 -u 192.168.1.107 9994
 
 
 
 # Depending on the type of outcome you want:
-# | netcat -w 1 -u 192.168.1.107 9994
+# | netcat -w=1 -u 192.168.1.107 9994
 # > /dev/ttys009
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/jgarr16/bash/main/jgspeed.sh)"
 # TODO: build in some fault tolerance so that when server 14204 isn't reachable, the script will wait for 30 minutes and then try again
